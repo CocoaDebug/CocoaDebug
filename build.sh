5968c4ddab26fac6b211926bf99c41166c1ee888
man.li <man.li@phicomm.com>
2018-03-12 16:09:46 +0800

    edit build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-BUILD_CMD="xcodebuild -scheme Example_Swift -sdk iphonesimulator build"
+BUILD_CMD="xcodebuild -project Example_Swift.xcodeproj -scheme Example_Swift -sdk iphonesimulator build"
 
 which -s xcpretty
 XCPRETTY_INSTALLED=$?
 
 if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
-  eval "${BUILD_CMD} | xcpretty"
+eval "${BUILD_CMD} | xcpretty"
 else
-  eval "$BUILD_CMD"
+eval "$BUILD_CMD"
 fi
+
