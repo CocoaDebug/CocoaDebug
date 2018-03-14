b135897791f323ca5d3653ec2afbfdd9500c0c89
man.li <man.li@phicomm.com>
2018-03-14 22:27:52 +0800

    unit test

---
+++ Example_ObjcTests/Example_ObjcTests.swift
@@ -0,0 +1,35 @@
+//
+//  Example_ObjcTests.swift
+//  Example_ObjcTests
+//
+//  Created by liman on 14/03/2018.
+//  Copyright © 2018 liman. All rights reserved.
+//
+
+import XCTest
+
+class Example_ObjcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
