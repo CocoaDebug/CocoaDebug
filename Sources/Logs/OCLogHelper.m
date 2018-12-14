ecec78540cd9b3b75cd1250101b8c3c993573936
man <723661989@163.com>
2018-12-14 19:06:47 +0800

    no message

--- Sources/Logs/OCLogHelper.m
+++ Sources/Logs/OCLogHelper.m
@@ -22,11 +22,23 @@
     return sharedInstance;
 }
 
+- (NSString *)parseFileInfo:(NSString *)file function:(NSString *)function line:(NSInteger)line
+{
+    NSString *fileName = [[file componentsSeparatedByString:@"/"] lastObject];
+    return [NSString stringWithFormat:@"%@[%ld]%@\n", fileName, (long)line, function];
+}
+
 - (void)handleLogWithFile:(NSString *)file function:(NSString *)function line:(NSInteger)line message:(NSString *)message color:(UIColor *)color
 {
     if (!self.enable) {
         return;
     }
+    
+    //1.
+    NSString *fileInfo = [self parseFileInfo:file function:function line:line];
+    
+    //2.
+
 }
 
 @end
