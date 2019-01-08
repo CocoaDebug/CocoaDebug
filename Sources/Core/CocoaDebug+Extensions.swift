66f3e56301931f56555d2879dd1ba95900f29f5f
man.li <man.li@shopee.com>
2019-01-08 11:53:40 +0800

    0.4.5

--- Sources/Core/CocoaDebug+Extensions.swift
+++ Sources/Core/CocoaDebug+Extensions.swift
@@ -229,13 +229,13 @@ extension UIWindow {
         return true
     }
     
-    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        super.motionEnded(motion, with: event)
-    }
-    
-    open override func motionCancelled(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        super.motionCancelled(motion, with: event)
-    }
+//    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+//        super.motionEnded(motion, with: event)
+//    }
+//
+//    open override func motionCancelled(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+//        super.motionCancelled(motion, with: event)
+//    }
     
     open override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         super.motionBegan(motion, with: event)
