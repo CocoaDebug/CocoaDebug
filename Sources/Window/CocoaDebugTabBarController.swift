5a9be3b8ebcd424d85f5d5700ac94017a76e30db
man.li <man.li@shopee.com>
2020-03-02 11:35:51 +0800

    tabbar index

--- Sources/Window/CocoaDebugTabBarController.swift
+++ Sources/Window/CocoaDebugTabBarController.swift
@@ -59,12 +59,12 @@ class CocoaDebugTabBarController: UITabBarController {
         
         //3.
         guard let tabBarControllers = CocoaDebugSettings.shared.tabBarControllers else {
-            self.viewControllers = [logs, network, sandbox, app]
+            self.viewControllers = [network, logs, sandbox, app]
             return
         }
         
         //4.添加额外的控制器
-        var temp = [logs, network, sandbox, app]
+        var temp = [network, logs, sandbox, app]
         
         for vc in tabBarControllers {
             
