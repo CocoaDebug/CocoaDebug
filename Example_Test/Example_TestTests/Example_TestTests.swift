3a3d43bcd900275b673011f470906e003d037b82
man.li <man.li@shopee.com>
2020-08-10 18:09:08 +0800

    .travis

---
+++ Example_Test/Example_TestTests/Example_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  Example_TestTests.swift
+//  Example_TestTests
+//
+//  Created by man on 8/10/20.
+//  Copyright © 2020 man. All rights reserved.
+//
+
+import XCTest
+
+class Example_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
