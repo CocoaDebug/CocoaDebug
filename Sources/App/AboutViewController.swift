f525496f49fde8d4be421888efa1b078ad75c2eb
man.li <man.li@shopee.com>
2020-08-25 20:16:42 +0800

    1.4.4

--- Sources/App/AboutViewController.swift
+++ Sources/App/AboutViewController.swift
@@ -28,7 +28,7 @@ class AboutViewController: UITableViewController {
         naviItem.titleView = naviItemTitleLabel
         
         
-        let version = "1.4.3"
+        let version = "1.4.4"
         
         self.versionLabel.text = "CocoaDebug Version ".appending(version)
         
